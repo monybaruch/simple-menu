76185ba4710c89894bff7fcae32cff093d9d6fe3
מוני ברוך <monybaruch@gmail.com>
2023-09-06 04:53:16 +0300

    adding a categories component and adding the option to filler the foods by a category

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import Title from './components/Title';
+import Menu from './components/Menu';
 import { useState } from 'react';
 import menu from './data';
-import Menu from './components/Menu';
+import Categories from './components/Categories';
+// const tempCategories = menu.map((item) => item.category);
+// const tempSet = new Set(tempCategories);
+// const tempItems = ['all', ...tempSet];
+// this is the long way
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))]; //short way
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    console.log(category); //for testing only
+    if (category === 'all') {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => {
+      if (item.category === category) {
+        return category;
+      }
+    });
+    setMenuItems(newItems);
+  };
   return (
     <main>
       <section>
         <Title text="Please Choose From Our delicious food menu:" />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
