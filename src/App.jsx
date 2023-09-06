9bccb850245cf3009147f9d34294b4f4e74fc817
מוני ברוך <monybaruch@gmail.com>
2023-09-06 04:04:38 +0300

    adding a menu and a menuItem components and setting the data for each food item

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Title from './components/Title';
-
+import { useState } from 'react';
+import menu from './data';
+import Menu from './components/Menu';
 const App = () => {
+  const [menuItems, setMenuItems] = useState(menu);
   return (
     <main>
       <section>
         <Title text="Please Choose From Our delicious food menu:" />
+        <Menu items={menuItems} />
       </section>
     </main>
   );
